7fa6d7a9d21c176946c5dcdb0e45968fba07aaaa
Moshe Holtzberg <moholtzberg@gmail.com>
2016-02-09 00:03:47 -0500

    enable smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -62,16 +62,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.smtp_settings = {
-  #   address:              "#{SECRET[MAILGUN_SMTP_ADDRESS]}",
-  #   port:                 587,
-  #   domain:               "#{SECRET[MAILGUN_SMTP_DOMAIN]}",
-  #   user_name:            "#{SECRET[MAILGUN_SMTP_UN]}",
-  #   password:             "#{SECRET[MAILGUN_SMTP_PW]}",
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "#{SECRET[MAILGUN_SMTP_ADDRESS]}",
+    port:                 587,
+    domain:               "#{SECRET[MAILGUN_SMTP_DOMAIN]}",
+    user_name:            "#{SECRET[MAILGUN_SMTP_UN]}",
+    password:             "#{SECRET[MAILGUN_SMTP_PW]}",
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
