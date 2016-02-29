3bda13e3d45ce273474c701f9d57c86cbfebc03a
Moshe Holtzberg <moholtzberg@gmail.com>
2016-02-29 01:17:21 -0500

    force to_f

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -6,30 +6,30 @@ class ShippingMethod < ActiveRecord::Base
     calculation = self.shipping_calculator.calculation_method
     puts "------> #{calculation}"
     if calculation == "flat"
-      amount = (rate >= self.minimum_amount) ? rate : self.minimum_amount
+      amount = (rate.to_f >= self.minimum_amount.to_f) ? rate.to_f : self.minimum_amount.to_f
       puts "1-------> #{amount}"
       amount = determine_free_shipping(amount)
       puts "2-------> #{amount}"
-      amount
+      amount.to_f
     elsif calculation == "percentage"
       amount = (rate/100) * order_amount
       puts "1-------> #{amount}"
-      amount = (rate >= self.minimum_amount) ? rate : self.minimum_amount
+      amount = (rate.to_f >= self.minimum_amount.to_f) ? rate.to_f : self.minimum_amount.to_f
       puts "2-------> #{amount}"
       amount = determine_free_shipping(amount)
       puts "3-------> #{amount}"
-      amount
+      amount.to_f
     end
     
   end
   
   def determine_free_shipping(amount)
     puts "XXX-------> #{amount}"
-    if self.free_at_amount < amount
+    if self.free_at_amount.to_f < amount.to_f
       puts "-------> #{self.free_at_amount}  =>  #{amount}"
       0
     else
-      amount
+      amount.to_f
     end
   end
   
