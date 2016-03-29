6afb5e06ea84c344620c8bdcdf39797108913469
Moshe Holtzberg <moholtzberg@gmail.com>
2016-03-28 21:37:43 -0400

    flush.cache after shipment

--- app/models/line_item_shipment.rb
+++ app/models/line_item_shipment.rb
@@ -3,4 +3,10 @@ class LineItemShipment < ActiveRecord::Base
   belongs_to :order_line_item, :touch => true
   belongs_to :shipment, :touch => true
   
+  after_commit :flush_cache
+  
+  def flush_cache
+    Rails.cache.delete("open_orders")
+  end
+  
 end
