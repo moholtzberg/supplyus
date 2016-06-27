244a96c4d0f34a43cec9e17f5f09149e5543067f
Moshe Holtzberg <moholtzberg@gmail.com>
2016-06-27 15:44:24 -0400

    fix syntax missing end

--- app/controllers/item_imports_controller.rb
+++ app/controllers/item_imports_controller.rb
@@ -12,6 +12,7 @@ class ItemImportsController < ApplicationController
     @item_import = ItemImport.new(params[:item_import])
     if @item_import.save
       redirect_to items_path, notice: "Imported items successfully."
+    end
     #Return error if file doesn't exist
     return redirect_to new_item_import_path, alert: "File is required." unless params[:item_import].present?
     import_hisotry = ImportHistory.create(nb_imported: 0, nb_failed:0, nb_in_queue: 0, is_processing: 1)
