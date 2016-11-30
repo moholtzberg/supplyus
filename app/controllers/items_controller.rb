4c2e7019e2cf78ae5b7bf83745fd8d01c7e02ecd
Moshe Holtzberg <moholtzberg@gmail.com>
2016-11-30 10:29:33 -0500

    lookup account_item_price from account_name instead of id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -105,7 +105,8 @@ class ItemsController < ApplicationController
   end
   
   def actual_price_by_item_number_and_account_id
-    @price = Item.find_by(number: params["item_number"]).actual_price(params["account_id"])
+    account_id = Account.find_by(name: params[:account_name]).id
+    @price = Item.find_by(number: params["item_number"]).actual_price(account_id)
     respond_to do |format|
       format.json {render :json => @price}
     end
