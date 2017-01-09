1eb15b1ef715fdcbb14829585eb627208d7a3d83
Moshe Holtzberg <moholtzberg@gmail.com>
2017-01-09 13:20:16 -0500

    set associated start alert to inactive, don't associate equipment to alert if no equipment exists

--- app/models/equipment_alert.rb
+++ app/models/equipment_alert.rb
@@ -2,9 +2,19 @@ class EquipmentAlert < ActiveRecord::Base
   
   belongs_to :equipment
   before_save :associate_equipement
+  after_save :set_start_alert_to_inactive
   
   def associate_equipement
-    self.equipment_id = Equipment.find_by(:serial => equipment_serial).id
+    self.equipment_id = Equipment.find_by(:serial => equipment_serial).id unless Equipment.find_by(:serial => equipment_serial).nil?
+  end
+  
+  def set_start_alert_to_inactive
+    if self.alert_type == "end"
+      a = EquipmentAlert.find_by(:alert_identification => self.alert_identification, :alert_type => "start")
+      if a
+        a.update_attributes(:active => false)
+      end
+    end
   end
   
 end
