377cef0753c2f5059f86ed10a3abcf1d65a85ee9
Moshe Holtzberg <moholtzberg@gmail.com>
2017-01-17 17:00:47 -0500

    equipment_alerts view and model updates

--- app/models/equipment_alert.rb
+++ app/models/equipment_alert.rb
@@ -1,8 +1,10 @@
 class EquipmentAlert < ActiveRecord::Base
   
   belongs_to :equipment
+  belongs_to :order_line_item
   before_save :associate_equipement
   after_save :set_start_alert_to_inactive
+  
   
   def associate_equipement
     self.equipment_id = Equipment.find_by(:serial => equipment_serial).id unless Equipment.find_by(:serial => equipment_serial).nil?
