b95fc507d1abb306fa2724b4a8c1f901c29aea37
Moshe Holtzberg <moholtzberg@gmail.com>
2017-01-20 17:18:31 +0000

    display if we are running jobs

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -35,58 +35,58 @@ if "#{SECRET['RUN_JOBS']}".present? and "#{SECRET['RUN_JOBS']}" == "true"
     puts "RUNNING A JOB"
   end
   
-  scheduler.every '1h' do
-    puts "********* Crating Orders from Equipment Alerts *********"
-    alerts = EquipmentAlert.where(:active => true, :alert_type => "start", :order_line_item_id => nil)
-    alerts.each do |alert|
-      if alert.equipment_id
-        equipment = alert.equipment
-        customer = equipment.customer
-        order = Order.where(:locked => nil, :completed_at => nil, :customer => equipment.account_id).first
-
-        if order.nil?
-          order = Order.new(:customer => equipment.customer)
-        end
-
-        order.notes = "Auto Supply Order"
-        order.sales_rep_id = customer.sales_rep_id
-        order.ship_to_address_1 = customer.ship_to_address_1
-        order.ship_to_address_2 = customer.ship_to_address_2
-        order.ship_to_city = customer.ship_to_city
-        order.ship_to_state = customer.ship_to_state
-        order.ship_to_zip = customer.ship_to_zip
-        order.email = customer.email
-        order.bill_to_address_1 = customer.bill_to_address_1
-        order.bill_to_address_2 = customer.bill_to_address_2
-        order.bill_to_city = customer.bill_to_city
-        order.bill_to_state = customer.bill_to_state
-        order.bill_to_zip = customer.bill_to_zip
-        order.bill_to_email = customer.bill_to_email
-        supply = equipment.find_supply(alert.supply_type, alert.supply_color)
-        unless supply.nil?
-          line = order.order_line_items.new
-          line.item_id = supply.item_id
-          line.quantity = 1
-          line.description = "ID# #{equipment.number} / #{equipment.location}"
-          line.price = line.item.get_lowest_price(equipment.account_id)
-          if line.save
-            alert.update_attributes(:active => false, :order_line_item_id => line.id)
-          end
-        end
-      end
-    end
-    puts "********* END *********"
-  end
+  # scheduler.every '1h' do
+ #    puts "********* Crating Orders from Equipment Alerts *********"
+ #    alerts = EquipmentAlert.where(:active => true, :alert_type => "start", :order_line_item_id => nil)
+ #    alerts.each do |alert|
+ #      if alert.equipment_id
+ #        equipment = alert.equipment
+ #        customer = equipment.customer
+ #        order = Order.where(:locked => nil, :completed_at => nil, :customer => equipment.account_id).first
+ #
+ #        if order.nil?
+ #          order = Order.new(:customer => equipment.customer)
+ #        end
+ #
+ #        order.notes = "Auto Supply Order"
+ #        order.sales_rep_id = customer.sales_rep_id
+ #        order.ship_to_address_1 = customer.ship_to_address_1
+ #        order.ship_to_address_2 = customer.ship_to_address_2
+ #        order.ship_to_city = customer.ship_to_city
+ #        order.ship_to_state = customer.ship_to_state
+ #        order.ship_to_zip = customer.ship_to_zip
+ #        order.email = customer.email
+ #        order.bill_to_address_1 = customer.bill_to_address_1
+ #        order.bill_to_address_2 = customer.bill_to_address_2
+ #        order.bill_to_city = customer.bill_to_city
+ #        order.bill_to_state = customer.bill_to_state
+ #        order.bill_to_zip = customer.bill_to_zip
+ #        order.bill_to_email = customer.bill_to_email
+ #        supply = equipment.find_supply(alert.supply_type, alert.supply_color)
+ #        unless supply.nil?
+ #          line = order.order_line_items.new
+ #          line.item_id = supply.item_id
+ #          line.quantity = 1
+ #          line.description = "ID# #{equipment.number} / #{equipment.location}"
+ #          line.price = line.item.get_lowest_price(equipment.account_id)
+ #          if line.save
+ #            alert.update_attributes(:active => false, :order_line_item_id => line.id)
+ #          end
+ #        end
+ #      end
+ #    end
+ #    puts "********* END *********"
+ #  end
   
   # scheduler.cron '30 16 * * *' do
-  scheduler.every '90m' do
-    puts "********* Completeting Incomplete Toner Orders from Alerts *********"
-    orders = Order.incomplete.where(:notes => "Auto Supply Order")
-    orders.each do |ord|
-      ord.update_attributes(:completed_at => DateTime.now)
-    end
-    puts "********* END *********"
-  end
+  # scheduler.every '90m' do
+  #   puts "********* Completeting Incomplete Toner Orders from Alerts *********"
+  #   orders = Order.incomplete.where(:notes => "Auto Supply Order")
+  #   orders.each do |ord|
+  #     ord.update_attributes(:completed_at => DateTime.now)
+  #   end
+  #   puts "********* END *********"
+  # end
   
   
 # scheduler.join
