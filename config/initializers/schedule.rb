5f72fe930aa0893a6c80ffd591a1b15430c33e2b
Moshe Holtzberg <moholtzberg@gmail.com>
2017-01-23 21:13:06 +0000

    remove limit 1 from complete incomplete orders schedule

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -85,7 +85,7 @@ if "#{SECRET['RUN_JOBS']}".present? and "#{SECRET['RUN_JOBS']}" == "true"
     j = Job.new
     puts "********* Completeting Incomplete Toner Orders from Alerts *********"
     j.job_name = "Complete Incomplete Toner Orders from Alerts"
-    orders = Order.is_incomplete.where(:notes => "Auto Supply Order").limit(1)
+    orders = Order.is_incomplete.where(:notes => "Auto Supply Order")
     orders.each do |ord|
       ord.update_attributes(:completed_at => DateTime.now)
       j.notes = "Sucess"
