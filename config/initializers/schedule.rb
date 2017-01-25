acd168e97ed118cb5a77102c8c8c0b0620d67653
Moshe Holtzberg <moholtzberg@gmail.com>
2017-01-25 02:03:14 +0000

    make rufus schedule run only once in production

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -2,7 +2,7 @@ require 'rufus/scheduler'
 
 if "#{SECRET['RUN_JOBS']}".present? and "#{SECRET['RUN_JOBS']}" == "true"
 
-  scheduler = Rufus::Scheduler.new
+  scheduler = Rufus::Scheduler.new(:lockfile => ".rufus-scheduler.lock", :overlap => false)
 
   # scheduler.every '1s' do
   # scheduler.every '1m' do
@@ -36,17 +36,27 @@ if "#{SECRET['RUN_JOBS']}".present? and "#{SECRET['RUN_JOBS']}" == "true"
     j = Job.new
     puts "********* Crating Orders from Equipment Alerts *********"
     j.job_name = "Crating Orders from Equipment Alerts"
+    j.notes = ""
     alerts = EquipmentAlert.where(:active => true, :alert_type => "start", :order_line_item_id => nil)
+    
     alerts.each do |alert|
+      j.notes += "[{alert_id: #{alert.id}}"
+      
       if alert.equipment_id
-        equipment = alert.equipment
-        customer = equipment.customer
-        order = Order.where(:locked => nil, :completed_at => nil, :customer => equipment.account_id).first
         
+        j.notes += "{equipment_id: #{alert.equipment_id}}"
+        equipment = alert.equipment
+        
+        j.notes += "{customer_id: #{equipment.customer.id unless equipment.nil?}}"
+        customer = equipment.customer
+        
+        order = Order.where(:locked => nil, :completed_at => nil, :customer => equipment.account_id).first
+      
         if order.nil?
-          order = Order.new(:customer => equipment.customer)
+          order = Order.new(:customer => customer)
         end
-  
+        j.notes += "{order_id: #{order.id}}"
+        
         order.notes = "Auto Supply Order"
         order.sales_rep_id = customer.sales_rep_id
         order.ship_to_account_name = customer.name
@@ -63,19 +73,28 @@ if "#{SECRET['RUN_JOBS']}".present? and "#{SECRET['RUN_JOBS']}" == "true"
         order.bill_to_state = customer.bill_to_state
         order.bill_to_zip = customer.bill_to_zip
         order.bill_to_email = customer.bill_to_email
+        
         supply = equipment.find_supply(alert.supply_type, alert.supply_color)
+        j.notes += "{supply: #{supply.item_id unless supply.nil?}}"
+        
         unless supply.nil?
           line = order.order_line_items.new
           line.item_id = supply.item_id
           line.quantity = 1
           line.description = "ID# #{equipment.number} / #{equipment.location}"
           line.price = line.item.get_lowest_price(equipment.account_id)
+          
           if line.save
             alert.update_attributes(:active => false, :order_line_item_id => line.id)
-            j.notes = "Sucess"
+            j.notes += "{status : sucess}]"
+          else
+            j.notes += "{status : fail}]"
           end
+          
         end
+        
       end
+    
     end
     puts "********* END *********"
     j.save
