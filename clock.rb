95c2d4f4ade220342b09f7bd6cd75a21e1468504
Moshe Holtzberg <moholtzberg@gmail.com>
2017-05-09 22:34:54 -0400

    move clock from /app directory to root of application, RAILS_ENV to Procfile

--- clock.rb
+++ clock.rb
@@ -1,6 +1,6 @@
 require 'clockwork'
-require_relative '../config/boot'
-require_relative '../config/environment'
+require_relative './config/boot'
+require_relative './config/environment'
 
 module Clockwork
   
