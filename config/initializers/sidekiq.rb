b80ebd8ed5525d6539f2a45c6f65c320193261cc
Moshe Holtzberg <moholtzberg@gmail.com>
2017-05-11 14:00:50 -0400

    sidekicks.rb use different reds db for production, add mailers queue to sidekicks.yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,21 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: "#{SECRET['REDIS_URL']}" }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "#{SECRET['REDIS_URL']}" }
+if Rails.env == "production"
+  
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "#{SECRET['REDIS_PROD_URL']}" }
+  end
+  
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "#{SECRET['REDIS_PROD_URL']}" }
+  end
+  
+else
+  
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "#{SECRET['REDIS_DEV_URL']}" }
+  end
+  
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "#{SECRET['REDIS_DEV_URL']}" }
+  end
+  
 end
