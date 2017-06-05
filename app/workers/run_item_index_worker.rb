cf983d2c380ff05b8e3b273a89b4b728dd197d34
Moshe Holtzberg <moholtzberg@gmail.com>
2017-06-05 01:02:30 -0400

    fix record not found in worker

--- app/workers/run_item_index_worker.rb
+++ app/workers/run_item_index_worker.rb
@@ -11,7 +11,7 @@ class RunItemIndexWorker
     puts "**** Start Time #{start_time}"
     ih.update_attributes(nb_in_queue: (start_id..end_id).size, is_processing: (start_id..end_id).size > 0, nb_imported: 0, nb_failed: 0)
     (start_id.to_i..end_id.to_i).each do |item_id|
-      Item.find(item_id).index_async
+      Item.find_by(id: item_id).index_async if Item.find_by(id: item_id)
     end
     end_time = Time.now
     puts "**** End Time #{end_time}"
