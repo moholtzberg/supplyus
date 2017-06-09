d8c17f86eef5b4c6abc41cb07681990450036e41
Moshe Holtzberg <moholtzberg@gmail.com>
2017-06-09 01:42:34 -0400

    redirect_to @item in html mode on items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,7 +57,9 @@ class ItemsController < ApplicationController
     @items = Item.all
     @items = @items.paginate(:page => params[:page], :per_page => 25)
     respond_to do |format|
-      format.html
+      format.html do
+        redirect_to @item
+      end
       format.js
     end
   end
@@ -80,7 +82,9 @@ class ItemsController < ApplicationController
     @items = Item.all
     @items = @items.paginate(:page => params[:page], :per_page => 25)
     respond_to do |format|
-      format.html
+      format.html do
+        redirect_to @item
+      end
       format.js
       format.json do 
         respond_with_bip(@item)
@@ -99,7 +103,7 @@ class ItemsController < ApplicationController
     e = Item.find_by(:id => params[:id])
     e.destroy!
     respond_to do |format|
-      format.js { alert("ITem Detelet") }
+      format.js { alert("Item Deleted") }
     end
   end
   
