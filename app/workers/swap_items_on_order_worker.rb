a8f8defaa1d4e7912a3e6cadba7e1f8fbaed5348
Moshe Holtzberg <moholtzberg@gmail.com>
2017-08-29 16:59:53 -0400

    add swap items on order worker

---
+++ app/workers/swap_items_on_order_worker.rb
@@ -0,0 +1,46 @@
+require 'sidekiq-scheduler'
+
+class SwapItemsOnOrderWorker
+  
+  include Sidekiq::Worker
+  
+  def perform(order_id)
+    order = Order.find(order_id)
+    if order.pending?
+      if order.account and order.account.replace_items == true
+        order.order_line_items.each do |line_item|
+          if line_item.item and (line_item.item.item_substitutes.size > 0)
+            subs = line_item.item.item_substitutes
+            price_to_beat = line_item.price
+            best_match = line_item.item_id
+            quantity = line_item.actual_quantity
+            puts "Quantity -> #{quantity}"
+            new_best_match = nil
+            subs.each do |sub|
+              if sub.replacement_item.actual_price(order.account_id) < price_to_beat
+                price_to_beat = sub.replacement_item.actual_price(order.account_id)
+                new_best_match = sub.replacement_item
+                quantity = (sub.original_uom_qty.to_i * line_item.actual_quantity.to_i).to_i / sub.replacement_uom_qty.to_i
+                puts "-> Quantity -> #{quantity}"
+                if quantity < 1
+                  quantity = 1
+                end
+              end
+            end
+            puts "BestMatch -> #{Item.find(best_match).item_vendor_prices.map(&:price).min}"
+            puts "NewBest Match -> #{new_best_match}"
+            if Item.find(new_best_match).actual_cost_price <= Item.find(best_match).actual_cost_price
+              line_item.quantity_canceled = line_item.quantity
+              new_line_item = order.order_line_items.new(item_id: new_best_match.id, quantity: quantity, price: new_best_match.actual_price(order.account_id))
+              puts "-------> new_line #{new_line_item.inspect}"
+              puts "-------> old_line #{line_item.inspect}"
+              line_item.save
+              new_line_item.save
+            end
+          end
+        end
+      end
+    end
+  end
+  
+end
