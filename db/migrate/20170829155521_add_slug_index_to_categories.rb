475aed0d9c381c1e831703b900cc498a88526ccb
Moshe Holtzberg <moholtzberg@gmail.com>
2017-11-15 16:55:23 -0500

    remove slug index migrations for item and category

--- db/migrate/20170829155521_add_slug_index_to_categories.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugIndexToCategories < ActiveRecord::Migration
-  def change
-    add_index :categories, :slug, :unique => true
-    Category.find_each(&:save)
-  end
-end
--- db/migrate/20170829155605_add_slug_index_to_items.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugIndexToItems < ActiveRecord::Migration
-  def change
-    add_index :items, :slug, :unique => true
-    Item.find_each(&:save)
-  end
-end
